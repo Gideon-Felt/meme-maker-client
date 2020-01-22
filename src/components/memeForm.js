89dae289cac3513cb5e3ad8b6dfc74b222c73866
Gideon-Felt <gideon@feltslanding.com>
2020-01-22 09:34:32 -0700

    added blank form for meme submission

--- src/components/memeForm.js
+++ src/components/memeForm.js
@@ -1,9 +1,43 @@
-import React, { useState, useEffect } from "react"
+import React, { useState, useEffect, useRef } from "react"
+import DropzoneComponent from "react-dropzone-component"
+import request from "superagent"
+import axios from "axios"
+import { navigate } from "hookrouter"
+
+import "../../node_modules/react-dropzone-component/styles/filepicker.css"
+import "../../node_modules/dropzone/dist/min/dropzone.min.css"
 
 function MemeForm(props) {
+    const [ text, setText ] = useState("")
+    const [ favorite, setFavorite ] = useState(false)
+    const [ image, setImage ] = useState("")
+    const imageRef = useRef(null)
+
     return (
         <div>
-            <h1>Meme Form</h1>
+            {props.editMode ?
+            <h1> Edit Meme</h1>
+            :
+            <h1>Add a Meme</h1>
+            }
+            <form>
+                <input
+                type="text"
+                placeholder="Caption"
+                value={text}
+                onChange={e => setText(e.target.value)}
+                />
+                <div>
+                    <input
+                    type="checkbox"
+                    checked={favorite}
+                    onChange={() => setFavorite(!favorite)}
+                    />
+                    <span>Favorite?</span>
+                </div>
+                <button type="submit">save meme</button>
+            </form>
+
         </div>
     )
 }
